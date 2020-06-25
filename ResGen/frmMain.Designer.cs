5be408b9f46b479b6ed49326dd01e6cc9e29f46c
RonanSMS <ronan.phuc.nguyen@somotsoftvn.com>
2020-06-25 11:14:13 +0700

    Create Resource Files Generator Utility tool

---
+++ ResGen/frmMain.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ResGen
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialogFileLoc = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.rtbLogs = new System.Windows.Forms.RichTextBox();
+            this.txtFileLoc = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resource files location:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(688, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(30, 25);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // rtbLogs
+            // 
+            this.rtbLogs.Enabled = false;
+            this.rtbLogs.Location = new System.Drawing.Point(8, 43);
+            this.rtbLogs.Name = "rtbLogs";
+            this.rtbLogs.Size = new System.Drawing.Size(836, 340);
+            this.rtbLogs.TabIndex = 2;
+            this.rtbLogs.Text = "";
+            // 
+            // txtFileLoc
+            // 
+            this.txtFileLoc.Enabled = false;
+            this.txtFileLoc.Location = new System.Drawing.Point(160, 12);
+            this.txtFileLoc.Name = "txtFileLoc";
+            this.txtFileLoc.Size = new System.Drawing.Size(522, 25);
+            this.txtFileLoc.TabIndex = 3;
+            this.txtFileLoc.Text = "D:\\Work\\New folder";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(724, 11);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(120, 25);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Generate Res";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 389);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtFileLoc);
+            this.Controls.Add(this.rtbLogs);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Resource Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogFileLoc;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.RichTextBox rtbLogs;
+        private System.Windows.Forms.TextBox txtFileLoc;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ ResGen/frmMain.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResGen
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialogFileLoc.ShowDialog() == DialogResult.OK)
+            {
+                txtFileLoc.Text = folderBrowserDialogFileLoc.SelectedPath;
+
+                if (IsValidLocation(txtFileLoc.Text))
+                {
+                    btnBrowse.Enabled = false;
+                    btnStart.Enabled = true;
+                }
+                else
+                    WriteLog(string.Format("'{0}' does not contain any *.resx file.", txtFileLoc.Text));
+            }
+        }
+
+        private bool IsValidLocation(string folderPath)
+        {
+            if (Directory.GetFiles(folderPath, "*.resx").Length == 0)
+                return false;
+
+            return true;
+        }
+
+        private void WriteLog(string msg)
+        {
+            if (string.IsNullOrEmpty(rtbLogs.Text))
+                rtbLogs.Text = msg;
+            else
+                rtbLogs.Text += "\n" + msg;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            List<string> files = GetAllResx(txtFileLoc.Text);
+            var proc = new ProcessStartInfo();
+            string anyCommand = "cd /d " + txtFileLoc.Text;
+            proc.UseShellExecute = true;
+            proc.WorkingDirectory = @"C:\Windows\System32";
+            proc.FileName = @"C:\Windows\System32\cmd.exe";
+            proc.Arguments = "/k " + anyCommand;
+
+            foreach (var file in files)
+            {
+                proc.Arguments += "&&ResGen.exe " + file;
+            }
+
+            proc.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(proc);
+            WriteLog("Done!");
+        }
+
+        private List<string> GetAllResx(string path)
+        {
+            List<string> files = new List<string>();
+
+            foreach (var item in Directory.GetFiles(path, "*.resx"))
+            {
+                files.Add(Path.GetFileName(item));
+            }
+
+            return files;
+        }
+    }
+}
+
